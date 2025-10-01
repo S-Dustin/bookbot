2f58a2be9166f09d3f8d11d53a8be70e9108fed4
S-Dustin <dustin.m.sauter@gmail.com>
2025-09-30 21:24:32 -0500

    Added srgs and stylized output

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_num_letters
 
 def get_book_text(file_path):
@@ -7,9 +8,25 @@ def get_book_text(file_path):
 
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    get_num_words(book_text)
-    get_num_letters(book_text)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    FILEPATH = "books/frankenstein.txt"
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    book_text = get_book_text(sys.argv[1])
+
+    print("----------- Word Count ----------")
+    words = get_num_words(book_text)
+    print(f"Found {len(words)} total words")
+
+    print("--------- Character Count -------")
+    letters = get_num_letters(book_text)
+    for letter in letters:
+        print(f"{letter['char']}: {letter['num']}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,18 @@
 def get_num_words(string):
     words = string.split()
-    print(f"Found {len(words)} total words")
+    return words
+
+def sort_on(items):
+    return items["num"]
+
+def sorted_letters(unsorted):
+    sorted_list = []
+    for letter in unsorted:
+        if letter.isalpha():
+            letter_dict = {"char": letter, "num": unsorted[letter]}
+            sorted_list.append(letter_dict)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 def get_num_letters(string):
     letter_count = {}
@@ -10,4 +22,4 @@ def get_num_letters(string):
             letter_count[letter] += 1
         else:
             letter_count[letter] = 1
-    print(letter_count)
+    return sorted_letters(letter_count)
